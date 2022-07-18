f8e8912ae2764c795c3fd413898ef994221928e7
Yunus emre Akyol <yemreakyl@gmail.com>
2022-07-18 16:46:42 +0300

    .

--- Script.js
+++ Script.js
@@ -2,8 +2,33 @@ const saveButton=document.querySelector('#saveBtn');
 const titleİnput=document.querySelector('#title');
 const descriptionİnput=document.querySelector('#description');
 const notesContainer=document.querySelector("#Notes");
+const deleteButton=document.querySelector("#deleteBtn");
+//Yeni not ekleme formunu temizleme
+function clearForm(){
+    titleİnput.value='';
+    descriptionİnput.value='';
+    deleteButton.classList.add("hidden");
+}
 
-//Yeni not ekleme işlemi için kullanacağım method
+//aşağı kısımdaki 3 method display notes içerisindeki event ile tetiklenip tıklanan not bilgilerini sol taraftaki bölüme getiriyor ayrıca var olan bir note seçildiğinde delete butonunu aktif ediyor
+function displayNoteInForm(note){
+titleİnput.value=note.title;
+descriptionİnput.value=note.description;
+deleteButton.classList.remove("hidden");
+deleteButton.setAttribute('data-id',note.id);
+};
+function populateForm(id){
+   getNotByİd(id);  
+};
+
+function getNotByİd(id){
+
+    fetch(`https://localhost:7284/api/Notes/${id}`).then(data=>data.json()).then(response=>displayNoteInForm(response));
+};
+
+
+
+//Yeni not ekleme işlemi için kullanacağım method ardından clearform() ve getnotes() methodlarımı çağırıyorum
 function addNote(title,description){
     const body={
         title: title,
@@ -18,7 +43,9 @@ function addNote(title,description){
     }
 })
 .then(data=>data.json())
-.then(response=>console.log(response));
+.then(response=>{clearForm();
+getNotes();
+});
 };
 
 
@@ -28,7 +55,7 @@ function displayNotes(notes){
     let allnotes='';
     notes.forEach(note => {
         const noteElement = `
-                            <div class="Note">
+                            <div class="Note" data-id="${note.id}">
                             <h3>${note.title}</h3> 
                             <p>${note.description}</p>
                             </div>
@@ -37,7 +64,13 @@ function displayNotes(notes){
         
     });
     notesContainer.innerHTML=allnotes
+    document.querySelectorAll(".Note").forEach(note=>{
+        note.addEventListener("click",function(){
+           populateForm(note.dataset.id)
+        });
+    });
     };
+    
 //Veri tabanından notlarımı çekip ekrana yazdırıyorum
 function getNotes(){
     fetch('https://localhost:7284/api/Notes')
@@ -46,8 +79,28 @@ function getNotes(){
 };
 getNotes();
 
+//Silme işlemleri için
+function deleteNotById(id){
+    fetch(`https://localhost:7284/api/Notes/${id}`,{
+        method:'DELETE',
+        headers:{
+            "content-type":"application/json"
+        }
+        })
+        .then(response=>{
+            clearForm();
+            getNotes();
+        })
+        ;
+   
+}
 //Save butonuna event ekliyorum
 saveButton.addEventListener('click',function(){
-    AddNote(titleİnput.value,descriptionİnput.value);
+    addNote(titleİnput.value,descriptionİnput.value);
     });
 
+ //Delete butonuna event ekliyorum   
+deleteButton.addEventListener('click',function(){
+const id=deleteButton.dataset.id;
+deleteNotById(id);
+});
